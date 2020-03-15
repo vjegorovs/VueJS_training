ed76fcf8fbc40af6c4d332b3993574bf6f41eb24
unknown <Jegorovsv@gmail.com>
2020-03-15 03:18:17 +0200

    Practised list rendering, event handling and class/style binding

--- main.js
+++ main.js
@@ -4,6 +4,30 @@ var app = new Vue({
         product: 'Socks',
         description: `They are real warm!`,
         image: "./assets/img/vmSocks-green.jpg",
-        inventory: 8
+        inventory: 8,
+        inStock: false,
+        details: ["80% cotton", "20% polyester", "Gender-neutral"],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: "green",
+                variantImage: "./assets/img/vmSocks-green.jpg"
+            },
+            {
+                variantId: 2235,
+                variantColor: "blue",
+                variantImage: "./assets/img/vmSocks-blue.jpg"
+            }
+        ],
+        cart: 0,
+        
+    },
+    methods: {
+        addToCart(){
+            this.cart += 1;
+        },
+        updateProduct(variantImage){
+            this.image = variantImage;
+        }
     }
 });
