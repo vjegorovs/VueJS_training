e390f691f2e42683414bcbd3cf9a4e5a70599179
unknown <Jegorovsv@gmail.com>
2020-03-15 02:42:15 +0200

    Created a simple image / description view, figured out attribute binding and contirionals

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-var product = 'Socks';
+var app = new Vue({
+    el: '#app',
+    data: {
+        product: 'Socks',
+        description: `They are real warm!`,
+        image: "./assets/img/vmSocks-green.jpg",
+        inventory: 8
+    }
+});
